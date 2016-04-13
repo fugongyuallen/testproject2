0fb7b0fb8b03ca116efb7a07869b518ee5b49c7f
GONGYU FU <gofu@ucsd.edu>
2016-04-13 11:48:43 -0700

    first commit

---
+++ app/src/androidTest/java/com/allensupercompany/testproject/tests/Junit_test.java
@@ -0,0 +1,25 @@
+package com.allensupercompany.testproject.tests;
+
+import android.test.ActivityInstrumentationTestCase2;
+import android.widget.TextView;
+
+import com.allensupercompany.testproject.MainActivity;
+import com.allensupercompany.testproject.R;
+
+/**
+ * Created by allenF on 4/13/16.
+ */
+public class Junit_test extends ActivityInstrumentationTestCase2<MainActivity>{
+
+    MainActivity mainActivity;
+    public Junit_test(){
+        super(MainActivity.class);
+    }
+
+    public void test_first(){
+        mainActivity = getActivity();
+        TextView textView = (TextView) mainActivity.findViewById(R.id.tv1);
+        String tester = textView.getText().toString();
+        assertEquals("Bye World",tester);
+    }
+}
